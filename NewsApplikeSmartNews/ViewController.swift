e9da55ecd79933a55ec0384e85f9b3f3e93a1e15
Junya Kobayashi <atsubeee@doi.ics.keio.ac.jp>
2018-03-29 02:14:19 +0900

    Initial Commit

---
+++ NewsApplikeSmartNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsApplikeSmartNews
+//
+//  Created by 小林純也 on 2018/03/29.
+//  Copyright © 2018年 Junya Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'NewsApplikeSmartNews' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+pod 'PageMenu'
+  # Pods for NewsApplikeSmartNews
+
+  target 'NewsApplikeSmartNewsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'NewsApplikeSmartNewsUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
